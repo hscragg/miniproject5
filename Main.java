e933c36e1cde0897ab209040c140d4dae1df98ae
hscragg <hayden.scragg@patelhs.org>
2021-02-26 14:04:39 +0000

    ok

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-     System.out.println("You are entering a car competetition, you must have atleast 4 cars to enter. How many cars do you have?");
+     System.out.println("You are entering a car competetition, you must have atleast 4 cars to enter. How many cars do you have??");
     int totalCars = scan.nextInt();
   }
     public static String cars(int num)
